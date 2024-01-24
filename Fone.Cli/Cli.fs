88f5e969ab4ccd20159ed9237ba5f5512b4acbe2
weebs <147543+weebs@users.noreply.github.com>
2024-01-24 13:28:06 -0600

    relative path + quicktest name

--- Fone.Cli/Cli.fs
+++ Fone.Cli/Cli.fs
@@ -242,9 +242,10 @@ let main argv =
 """
     let testsProjFile =
         // Path.Join(__SOURCE_DIRECTORY__, "../tests/Fable.Tests.C/Fable.Tests.C.fsproj")
-        "C:/Users/Dave/projects/Fable/src/quicktest/Quicktest.fsproj"
-    File.WriteAllText("/Quicktest.fsproj", projText)
+        Path.Join(__SOURCE_DIRECTORY__, "../src/quicktest/Quicktest.fsproj")
+        // "C:/Users/Dave/projects/Fable/src/quicktest/Quicktest.fsproj"
+    // File.WriteAllText("/Quicktest.fsproj", projText)
     // let quicktest = Path.Join(__SOURCE_DIRECTORY__, "../tests/Fable.Tests.C/Fable.Tests.C.fsproj")
     // Compiler.compileSingleFile projFile // "C:/Users/Dave/projects/Fable/src/quicktest/QuickTest.fs"
-    Compiler.compileProject "/Quicktest.fsproj"
+    Compiler.compileProject testsProjFile
     0
