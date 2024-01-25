0c32be8b6e0f585a5e1248b06af0778a5e3c2685
weebs <147543+weebs@users.noreply.github.com>
2024-01-25 16:10:42 -0600

    move writeUnion code

--- Fone.Cli/DataType/DataType.fs
+++ Fone.Cli/DataType/DataType.fs
@@ -3,6 +3,7 @@ module Fone.DataType.Core
 open Fable
 open Fable.AST
 open Fable.AST.Fable
+open Fable.C
 open Fable.C.AST
 open System.Text
 
@@ -43,3 +44,93 @@ let writeStruct (ent: Entity) (struct_name: string, compiledFields: (string * C.
             _sb.AppendLine $"{struct_name}* {struct_name}_ctor();" |> ignore
             _sb.AppendLine $"void {struct_name}_Destructor({struct_name}* this$);" |> ignore
         _sb.ToString()
+
+let writeUnionToBuilder (sb: CompiledOutputBuilder) (ent: Entity) =
+                    let getCaseInfo (case: UnionCase) : string * (string * C.Type) list =
+                        let fields = case.UnionCaseFields
+                        case.FullName.Replace(".", "_"), [
+                            for f in fields do
+    //                            let f2 = f :?> FsField
+                                // yield (f.DisplayName, transformType [] f.FieldType)
+                                // todo: merp
+                                yield (f.Name, Transforms.transformType [] f.FieldType)
+                        ]
+                    let cases =
+                        ent.UnionCases
+                        |> List.map getCaseInfo
+                        |> List.map (writeStruct ent)
+                    let fieldNames = ent.UnionCases |> List.map (fun c -> $"""{c.FullName.Replace(".", "_")} {c.Name};""")
+                    let fullName = ent.FullName.Replace(".", "_")
+                    let union = $"""
+typedef union {fullName}_UnionData {{
+    {System.String.Join("\n    ", fieldNames)}
+}} {fullName}_UnionData;
+typedef struct {fullName} {{
+    unsigned char __refcount;
+    int union_tag;
+    union {fullName}_UnionData union_data;
+}} {fullName};
+"""
+// typedef union {fullName}_UnionData {{
+//     {System.String.Join("\n    ", fieldNames)}
+// }} {fullName}_UnionData;
+// typedef struct {fullName} {{
+//     int union_tag;
+//     union {fullName}_UnionData union_data;
+// }} {fullName};
+//                    cases |> List.iter (fun (name, fields) -> writeStruct ent name fields)
+                    cases |> List.iter (sb.AppendLine >> ignore)
+                    sb.AppendLine union |> ignore
+                    if not ent.IsValueType then
+                        let cleanup = [|
+                            for tag in 0..ent.UnionCases.Length - 1 do
+                                let case = ent.UnionCases[tag]
+                                let toCall = [|
+                                    for field in case.UnionCaseFields do
+                                        if Transforms.requiresTracking [] field.FieldType then
+                                            let t = Transforms.transformType [] field.FieldType
+                                            let name =
+                                                match t with
+                                                | C.Ptr t -> t.ToNameString()
+                                                | t -> t.ToNameString()
+                                            $"    Runtime_end_var_scope(this$->union_data.{case.Name}.{field.Name}, {name}_Destructor);"
+                                |]
+                                if toCall.Length > 0 then
+                                    $"if ({tag} == this$->union_tag) {{\n" +
+                                    System.String.Join("\n    else ", toCall) + "\n    }"
+                        |]
+                        let finalizer = $"""
+void {fullName}_Destructor({fullName}* this$) {{
+    {cleanup |> String.concat "\n"}
+    free(this$);
+}}"""
+                        sb.AppendLine finalizer
+                        |> ignore
+                        let caseConstructors = [
+                            for (tag, case) in ent.UnionCases |> List.mapi (fun index case -> index, case) do
+                                let info = getCaseInfo case
+                                let argText (arg: Field) =
+                                    let t = Transforms.transformType [] arg.FieldType
+                                    $"{t.ToTypeString()} {arg.Name}"
+                                let args = case.UnionCaseFields |> List.map argText |> String.concat ", "
+                                let assignments =
+                                    case.UnionCaseFields
+                                    |> List.map (fun field ->
+                                        $"this$->union_data.{case.Name}.{field.Name} = {field.Name};" +
+                                        (if Transforms.requiresTracking [] field.FieldType then
+                                            $"\n    this$->union_data.{case.Name}.{field.Name}->__refcount++;"
+                                         else
+                                            "")
+                                    )
+                                let name = $"{fullName}_{case.Name}_ctor"
+                                $"""
+{fullName}* {name}({args}) {{
+    {fullName}* this$ = ({fullName}*)malloc(sizeof({fullName}));
+    this$->__refcount = 1;
+    this$->union_tag = {tag};
+    {assignments |> String.concat "\n    "}
+    return Runtime_autorelease(this$, {fullName}_Destructor);
+}}
+"""
+                        ]
+                        sb.AppendLine (caseConstructors |> String.concat "\n") |> ignore
