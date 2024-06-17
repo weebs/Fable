33f3e98921f6dc50fbf196ac6965f009f127086b
weebs <147543+weebs@users.noreply.github.com>
2024-06-17 14:09:56 -0500

    cleanup fable

--- Fone.Cli/Mixin.fs
+++
@@ -1,8 +0,0 @@
-﻿namespace global
-
-[<AutoOpen>]
-module Mixin =
-    let debug = true
-    let printfn fmt = Printf.kprintf (fun str ->
-  // Output the formatted string if 'debug', otherwise do nothing
-        if debug then printfn "%s" str) fmt
